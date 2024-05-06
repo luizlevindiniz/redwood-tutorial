3ac8fb4c6d142d45850504d75175207eacebc333
Luiz Diniz <luizd97@gmail.com>
2024-05-06 19:32:55 -0300

    first commit

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+      <h2>About us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        aperiam a quaerat dicta odio vel veniam ex nam. Impedit architecto
+        blanditiis explicabo vel debitis qui adipisci reiciendis quasi sed
+        libero!
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
